ef9455d0bd108ad09858c6c526cc840bfd0580e5
Thanatip Tuntapalin <thanatip.t@tcc-technology.com>
2026-02-02 22:34:25 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,8 +4,9 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
-    pass
-
+    command = input("$")
+    if command:
+        sys.stdout.write(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
