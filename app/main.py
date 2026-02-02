a8ccfe73a033717f651eb16b3919b2d45e064a68
Thanatip Tuntapalin <thanatip.t@tcc-technology.com>
2026-02-02 22:34:55 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ import sys
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
-    sys.stdout.write("$ ")
-    command = input("$")
+    sys.stdout.write("")
+    command = input()
     if command:
         sys.stdout.write(f"{command}: command not found")
 
