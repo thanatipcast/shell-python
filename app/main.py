9377a35b960b03fa95b5293cf3dda6fb6273c557
Thanatip Tuntapalin <thanatip.t@tcc-technology.com>
2026-02-02 23:07:12 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,19 +6,16 @@ def main():
     while True: 
         command = input("$ ")
         command_split = command.split()
-        command_length = len(command_split)
         command_name = command_split[0]
         args = command_split[1:]
-        if len(command_split) == 1:
-            if command == "exit":
-                sys.exit()
-        elif command_length > 1:
-            if command_name == "echo":
-                    print(" ".join(args))
-            elif command:
-                sys.stdout.write(f"{command}: command not found")
-                print()
-                continue   
+        if command_name == "exit":
+            sys.exit()
+        elif command_name == "echo":
+                print(" ".join(args))
+        elif command:
+            sys.stdout.write(f"{command}: command not found")
+            print()
+            continue   
    
 
 if __name__ == "__main__":
