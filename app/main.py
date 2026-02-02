f41a0b962b0eb80baf91b28d1bf4b392b127612f
Thanatip Tuntapalin <thanatip.t@tcc-technology.com>
2026-02-02 22:50:00 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,8 +5,12 @@ def main():
     # TODO: Uncomment the code below to pass the first stage
     while True: 
         command = input("$ ")
+        print("argv",sys.argv)
+        print("argv1",sys.argv[1])
+        print("argv2", sys.argv[2])
         if command == "exit":
             sys.exit()
+
         elif command:
             sys.stdout.write(f"{command}: command not found")
             print()
