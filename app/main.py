15efb35a747a4cab7c46f4cfb16c83481d650c65
Thanatip Tuntapalin <thanatip.t@tcc-technology.com>
2026-02-02 23:05:31 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,16 +5,20 @@ def main():
     # TODO: Uncomment the code below to pass the first stage
     while True: 
         command = input("$ ")
-        print("argv",sys.argv)
-        print("argv1",sys.argv[1])
-        print("argv2", sys.argv[2])
-        if command == "exit":
-            sys.exit()
-
-        elif command:
-            sys.stdout.write(f"{command}: command not found")
-            print()
-            continue   
+        command_split = command.split()
+        command_length = len(command_split)
+        command_name = command_split[0]
+        args = command_split[1:]
+        if len(command_split) == 1:
+            if command == "exit":
+                sys.exit()
+        elif command_length > 1:
+            if command_name == "echo":
+                    print(" ".join(args))
+            elif command:
+                sys.stdout.write(f"{command}: command not found")
+                print()
+                continue   
    
 
 if __name__ == "__main__":
