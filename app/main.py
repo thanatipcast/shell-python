c39eb32b88c998ea442d9203310a3ffda8e5b349
Thanatip Tuntapalin <thanatip.t@tcc-technology.com>
2026-02-04 00:12:25 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import sys
-
+import os
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
@@ -13,11 +13,24 @@ def main():
             sys.exit()
         elif command_name == "echo":
                 print(" ".join(args))
-        elif command_name == "type":
+        elif command_name == "type" and len(args) == 1:
+            #TODO bug on command "type"
             if args[0] in shell_builtin:
                 print(f"{args[0]} is a shell builtin")
             else:
-                print(f"{args[0]}: not found")
+                found = False
+                path_env = os.environ.get("PATH","")
+                path_split = path_env.split(os.pathsep)
+                for directory in path_split:
+                    full_path = os.path.join(directory,args[0])
+                    full_path = full_path + ".exe"
+                    print("full_path", full_path)
+                    if os.path.isfile(full_path) and os.access(full_path,os.X_OK):
+                        print(f"{args[0]} is {full_path}")
+                        found = True
+                        break       
+                if not found:
+                    print(f"{args[0]}: not found")
 
         elif command:
             sys.stdout.write(f"{command}: command not found")
